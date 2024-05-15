a509638c4877934abb12fb0e6fbd9d6d5009ef89
1234Christina <ctina89h@gmail.com>
2024-05-15 11:42:22 +0000

    Add structural commenting to index.js

--- index.js
+++ index.js
@@ -1,6 +1,42 @@
-""" Put this inside an event handler """
+// /** The initial game functions */
 
-function working() {
-    firstDiv = getElementById("testDiv");
-    firstDiv.innerText = "It Works!"
-}
+// When page loads do this
+//
+
+// trueIsRight() {
+
+// };
+
+// trueIsWrong() {
+
+// };
+
+// When True is clicked do this
+//
+
+// falseIsRight() {
+
+// };
+
+// falseIsWrong() {
+
+// };
+
+// When False is clicked do this
+//
+
+
+// When user answer is confirmed as right (bool true) run this to track right answers out of total questions
+// addRightAnswers() {
+
+// };
+
+// When question has been answered move to this function
+// nextQuestionBtn() {
+
+// };
+
+// When all questions have been answered move to this funtion
+// showRightAnswers() {
+
+// };
