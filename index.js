9dbd5c2f39cb4eb74a401dd2a040306dfc50f5dd
1234Christina <ctina89h@gmail.com>
2024-05-16 08:59:24 +0000

    index.js:Add question bank for iteration

--- index.js
+++ index.js
@@ -3,6 +3,33 @@
 // When page loads do this
 //
 
+// A question bank to store all the possible questions in the game
+
+const questionBank = [
+    {
+        'question': 'The main character is called Lincoln Loud?',
+        'answer': true
+    },
+    {
+        'question': 'Lincoln Loud has ten sisters?',
+        'answer': true
+    },
+    {
+        'question': 'Lola and Lana are twin sisters?',
+        'answer': true
+    },
+    {
+        'question': 'Lincoln and Clyde are twin brothers?',
+        'answer': false //best friends
+    },
+    {
+        'question': 'Lucy Loud is the eldest sister?',
+        'answer': false //Lori is eldest sister
+    }
+
+    // Add as many as possible
+]
+
 // trueIsRight() {
 
 // };
